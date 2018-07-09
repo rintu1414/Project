3d46f2c2130a7da0c66342cbc099f5901ba7f6bd
Gantayat <email@example.com>
2018-07-09 18:29:15 -0400

    Added Basic XLS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import * as XLSX from 'xlsx';
+import {read, write, utils} from 'xlsx';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +9,52 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-  data = {
-    'classifications': [
-      {'name1': 'text', 'name2': 'text2', 'name3': 'text2'},
-      {'name1': 'text', 'name2': 'text2', 'name3': 'text2'},
-      {'name1': 'text', 'name2': 'text2', 'name3': 'text2'}]
-  };
+  arrayBuffer: any;
+  file: File;
+  data =
+    [
+      {
+        'ID': '001',
+        'Name': 'Eurasian Collared-Dove',
+        'Type': 'Dove',
+        'Status': 'Streptopelia'
+      },
+      {
+        'ID': '002',
+        'Name': 'Bald Eagle',
+        'Type': 'Hawk',
+        'Status': 'Haliaeetus leucocephalus'
+      },
+      {
+        'ID': '003',
+        'Name': 'Cooper\'s Hawk',
+        'Type': 'Hawk',
+        'Status': 'Accipiter cooperii'
+      }
+    ];
+
+
+  incomingfile(event) {
+    this.file = event.target.files[0];
+  }
+
+  Upload() {
+    let fileReader = new FileReader();
+    fileReader.onload = (e) => {
+      this.arrayBuffer = fileReader.result;
+      var data = new Uint8Array(this.arrayBuffer);
+      var arr = new Array();
+      for (var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+      var bstr = arr.join('');
+      var workbook = XLSX.read(bstr, {type: 'binary'});
+      var first_sheet_name = workbook.SheetNames[0];
+      var worksheet = workbook.Sheets[first_sheet_name];
+      console.log(XLSX.utils.sheet_to_json(worksheet, {raw: true}));
+      this.data = XLSX.utils.sheet_to_json(worksheet, {raw: true});
+      console.log(this.data);
+
+    };
+    fileReader.readAsArrayBuffer(this.file);
+  }
 
 }
