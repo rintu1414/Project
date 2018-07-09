fda9d3cbbf01fa4bd6ac9ff9cde06bb1ca71564e
Gantayat <email@example.com>
2018-07-09 19:13:45 -0400

    Service Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import * as XLSX from 'xlsx';
 import {read, write, utils} from 'xlsx';
+import {ExcelUploadService} from './excel-upload.service';
 
 @Component({
   selector: 'app-root',
@@ -33,28 +34,16 @@ export class AppComponent {
       }
     ];
 
+  constructor(private excelUploadService: ExcelUploadService) {
+
+  }
 
   incomingfile(event) {
     this.file = event.target.files[0];
+
   }
-
-  Upload() {
-    let fileReader = new FileReader();
-    fileReader.onload = (e) => {
-      this.arrayBuffer = fileReader.result;
-      var data = new Uint8Array(this.arrayBuffer);
-      var arr = new Array();
-      for (var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
-      var bstr = arr.join('');
-      var workbook = XLSX.read(bstr, {type: 'binary'});
-      var first_sheet_name = workbook.SheetNames[0];
-      var worksheet = workbook.Sheets[first_sheet_name];
-      console.log(XLSX.utils.sheet_to_json(worksheet, {raw: true}));
-      this.data = XLSX.utils.sheet_to_json(worksheet, {raw: true});
-      console.log(this.data);
-
-    };
-    fileReader.readAsArrayBuffer(this.file);
+  uploadExcel() {
+    this.excelUploadService.Upload();
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {ExcelUploadService} from './excel-upload.service';
 
 
 @NgModule({
@@ -12,7 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ExcelUploadService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
