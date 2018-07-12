abcd5b0806f9a17ca4f35b2434c0cceb183df941
Gantayat <email@example.com>
2018-07-12 17:11:55 -0400

    Progress Bar Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,26 @@ import { AppComponent } from './app.component';
 import {ExcelUploadService} from './excel-upload.service';
 import { UploadComponent } from './upload/upload.component';
 import { DataviewComponent } from './dataview/dataview.component';
-import { DataTableModule } from 'angular-6-datatable';
+import {DataTableModule} from 'angular-6-datatable';
+import { NgProgressModule } from 'ngx-progressbar';
 import { MatDialog, MatDialogModule } from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     UploadComponent,
-    DataviewComponent
+    DataviewComponent,
   ],
   imports: [
     BrowserModule,
     DataTableModule,
-    MatDialogModule
+    MatDialogModule,
+    NgProgressModule,
+    BrowserAnimationsModule
   ],
   providers: [ExcelUploadService, MatDialog],
+  entryComponents: [UploadComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
